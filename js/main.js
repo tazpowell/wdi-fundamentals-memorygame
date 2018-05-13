aaec4ba436ff7be19b52ec84f551cd28e65f2711
Tazuko Sugajima <tazuko@Stormageddon.local>
2018-05-13 13:28:08 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay .push(cards[0]);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay .push(cards[2]);
+console.log("User flipped king")
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");	
+	} else {
+		alert("Sorry, try again.");
+	}
+}
