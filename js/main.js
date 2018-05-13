763fcccc9dd23926636a1e05d8f8390733556516
Tazuko Powell <tazuko@Stormageddon.local>
2018-05-13 16:29:03 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,18 +3,23 @@ console.log("Up and running!");
 
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay .push(cards[0]);
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-cardsInPlay .push(cards[2]);
-console.log("User flipped king")
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");	
-	} else {
-		alert("Sorry, try again.");
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");	
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();	
+}	
+
+flipCard(0);
+flipCard(2);
+
+
